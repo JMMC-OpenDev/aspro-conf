7bd4c9f39878616a221cdc879e1243a8186bb441
bourgesl <bourges.laurent@gmail.com>
2013-02-13 14:47:39 +0000

    added dummy AsproConf interface to build (makefile)

---
+++ src/fr/jmmc/aspro/conf/AsproConf.java
@@ -0,0 +1,12 @@
+/*******************************************************************************
+ * JMMC project ( http://www.jmmc.fr ) - Copyright (C) CNRS.
+ ******************************************************************************/
+package fr.jmmc.aspro.conf;
+
+/**
+ * This interface is useless but required by MKF makefile
+ * @author bourgesl
+ */
+public interface AsproConf {
+    // nothing to do
+}
